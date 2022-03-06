dcaf0f2c8a6f0dcfe8aebedc86d0f044b86360ec
Nabery0z <93230006+Nabery0z@users.noreply.github.com>
2022-03-05 21:53:20 -0300

    now using nodejs and express to do backend

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const express = require("express")
 const app = express()
 
 
+app.get('/',function(req,res){
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get("/:jornal&:dej1&:dej2&:dej3&:dej4", function(req,res){
+    res.send(req.params)
+})
+
+app.listen(8080, function(){
+    console.log("Server has started")
+})
